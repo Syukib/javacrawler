1891c483d45b83fb2cfdd156daabc6a02c36cf82
Syukib <89130000+Syukib@users.noreply.github.com>
2021-09-18 20:57:35 -0400

    Update htmlunit_test.java

--- src/main/java/com/crawler/test/htmlunit_test.java
+++ src/main/java/com/crawler/test/htmlunit_test.java
@@ -12,26 +12,85 @@ import com.gargoylesoftware.htmlunit.html.HtmlPage;
 
 public class htmlunit_test {
 	//for github actions testing
-	private static Path readmePath = Paths.get("README.md");
+        
+	private static void write (String Publisher, String Product, String Version, String Build) throws IOException {
+        Files.write(product_result_data, "\"".getBytes(), StandardOpenOption.APPEND);
+        Files.write(product_result_data, Publisher.getBytes(), StandardOpenOption.APPEND);
+        Files.write(product_result_data, "\"".getBytes(), StandardOpenOption.APPEND);
+        Files.write(product_result_data, ",".getBytes(), StandardOpenOption.APPEND);
+        Files.write(product_result_data, "\"".getBytes(), StandardOpenOption.APPEND);
+        Files.write(product_result_data, Product.getBytes(), StandardOpenOption.APPEND);
+        Files.write(product_result_data, "\"".getBytes(), StandardOpenOption.APPEND);
+        Files.write(product_result_data, ",".getBytes(), StandardOpenOption.APPEND);
+        Files.write(product_result_data, "\"".getBytes(), StandardOpenOption.APPEND);
+        Files.write(product_result_data, Version.getBytes(), StandardOpenOption.APPEND);
+        Files.write(product_result_data, "\"".getBytes(), StandardOpenOption.APPEND);
+        Files.write(product_result_data, ",".getBytes(), StandardOpenOption.APPEND);
+        Files.write(product_result_data, "\"".getBytes(), StandardOpenOption.APPEND);
+        Files.write(product_result_data, Build.getBytes(), StandardOpenOption.APPEND);
+        Files.write(product_result_data, "\"".getBytes(), StandardOpenOption.APPEND);
+        Files.write(product_result_data, "\n".getBytes(), StandardOpenOption.APPEND);
+	};
 	
-	
-	public static void main(String[] args) throws IOException {
-		String url = "https://www.magicaudiotools.com/";
-		String product_name = "";
-		HtmlPage EntrancePage = CrawlerUtils.getPage(url);
-		//the hierarchy is //div/h3, then do filters on the div part, here the class within the div must contain the string 't_'
-		List <HtmlElement> infoListEle = (List <HtmlElement>)  EntrancePage.getByXPath("//ul[@class='menu']/li/a");
+	private static void go (String url, String Publisher, String xPath) {
+        HtmlPage EntrancePage = CrawlerUtils.getPage(url);
+		List <HtmlElement> infoListEle = (List <HtmlElement>)  EntrancePage.getByXPath(xPath);
 		for (int i = 0; i < infoListEle.size(); i++) {
-			product_name = infoListEle.get(i).getTextContent().trim();
-			if (product_name.length()!= 0) {
-				System.out.println("Product:" + product_name);
-		        if (!Files.exists(readmePath)) {
-		            Files.createFile(readmePath);
-		        }
-		        Files.write(readmePath, product_name.getBytes(), StandardOpenOption.APPEND);
+			String Product = infoListEle.get(i).getTextContent();
+			System.out.println("Publisher: "+ Publisher + " | " + "Product: " + Product);
+			try {
+				write (Publisher, Product, "", "");
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
 			}
 		}
 	}
 	
+	private static Path product_result_data = Paths.get("product_result_data.csv");
+	
+	public static void main(String[] args) throws IOException {
+		String url = "";
+		String Publisher = "";
+		String Product = "";
+		String Version = "";
+		String Build = "";
+		String xPath = "";
+		
+        if (Files.exists(product_result_data)) {
+        	Files.delete (product_result_data);
+            Files.createFile(product_result_data);
+        }
+        
+        Files.write(product_result_data, "\"".getBytes(), StandardOpenOption.APPEND);
+        Files.write(product_result_data, "Publisher".getBytes(), StandardOpenOption.APPEND);
+        Files.write(product_result_data, "\"".getBytes(), StandardOpenOption.APPEND);
+        Files.write(product_result_data, ",".getBytes(), StandardOpenOption.APPEND);
+        Files.write(product_result_data, "\"".getBytes(), StandardOpenOption.APPEND);
+        Files.write(product_result_data, "Product".getBytes(), StandardOpenOption.APPEND);
+        Files.write(product_result_data, "\"".getBytes(), StandardOpenOption.APPEND);
+        Files.write(product_result_data, ",".getBytes(), StandardOpenOption.APPEND);
+        Files.write(product_result_data, "\"".getBytes(), StandardOpenOption.APPEND);
+        Files.write(product_result_data, "Version".getBytes(), StandardOpenOption.APPEND);
+        Files.write(product_result_data, "\"".getBytes(), StandardOpenOption.APPEND);
+        Files.write(product_result_data, ",".getBytes(), StandardOpenOption.APPEND);
+        Files.write(product_result_data, "\"".getBytes(), StandardOpenOption.APPEND);
+        Files.write(product_result_data, "Build".getBytes(), StandardOpenOption.APPEND);
+        Files.write(product_result_data, "\"".getBytes(), StandardOpenOption.APPEND);
+        Files.write(product_result_data, "\n".getBytes(), StandardOpenOption.APPEND);
+        
+        //1, Thomson Software Solutions
+        Publisher = "Thomson Software Solutions";
+        url = "http://www.thomson-software-solutions.com/software-list/";
+        xPath = "//div[@class='fl-rich-text']/h2";
+        go (url, Publisher, xPath);
+        
+		//2, SBK Softwareb¨¹ro Kasparek
+        Publisher = "SBK Softwareb¨¹ro Kasparek";
+        url = "http://www.sbk-software.de/";
+        xPath = "//table[@id='menu']/tbody/tr/td/a";
+        go (url, Publisher, xPath);
+        
+	}//main end
 
 }
