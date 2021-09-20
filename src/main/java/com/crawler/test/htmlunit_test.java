85fe940d5dda5698daae54d882a692c41ccc09ba
Syukib <89130000+Syukib@users.noreply.github.com>
2021-09-20 19:31:50 -0400

    Update htmlunit_test.java

--- src/main/java/com/crawler/test/htmlunit_test.java
+++ src/main/java/com/crawler/test/htmlunit_test.java
@@ -85,12 +85,60 @@ public class htmlunit_test {
         xPath = "//div[@class='fl-rich-text']/h2";
         go (url, Publisher, xPath);
         
-		//2, SBK Softwareb¨¹ro Kasparek
+	//2, SBK Softwareb¨¹ro Kasparek
         Publisher = "SBK Softwareb¨¹ro Kasparek";
         url = "http://www.sbk-software.de/";
         xPath = "//table[@id='menu']/tbody/tr/td/a";
         go (url, Publisher, xPath);
+	
+	//3, Red Oak Software
+        Publisher = "Red Oak Software";
+        url = "http://www.redoaksw.com/products/";
+        xPath = "//table[@class='LayoutTable']/tbody/tr/td/h5";
+        go (url, Publisher, xPath);
         
+        //4, Recool Studio
+        Publisher = "Recool Studio";
+        url = "http://www.swf-video.com/download.htm";
+        xPath = "//tr[@class='order_pad']/td[1]/a";
+        go (url, Publisher, xPath);
+        
+        //5, Probit Software
+        Publisher = "Probit Software";
+        url = "http://www.probitsoftware.com/products.php";
+        xPath = "//div[contains(@class,'t_')]/h3";
+        go (url, Publisher, xPath);
+        
+        //6, Newera Software Technology
+        Publisher = "Newera Software Technology";
+        url = "http://www.iconcool.com/products.htm";
+        xPath = "//div[@class='box_con arrow']/ul/li";
+        go (url, Publisher, xPath);
+        
+        //7, Michael Bauer Software 
+        Publisher = "Michael Bauer Software";
+        url = "http://www.vboffice.net/en/products";
+        xPath = "//div[@class='content']/table/tbody/tr/td[2]";
+        go (url, Publisher, xPath);
+        
+        //8, Metaware
+        Publisher = "Metaware";
+        url = "http://www.metaware.fr/products.html";
+        xPath = "//ul[@class='nav nav-list primary push-bottom active-class-nav']/li/a";
+        go (url, Publisher, xPath);
+        
+        //9, PrivacyAnywhere Software
+        Publisher = "PrivacyAnywhere Software";
+        url = "http://www.privacyanywhere.com/downloads/";
+        xPath = "//a[contains(@class,'Blue')]/b";
+        go (url, Publisher, xPath);
+        
+        //10, Xilisoft Corporation
+        Publisher = "Xilisoft Corporation";
+        url = "http://www.xilisoft.com/products-all.html";
+        xPath = "//div[@id='sub-product-all-wraper']/div/div/div/div/p/a";
+        go (url, Publisher, xPath);
+
 	}//main end
 
 }
